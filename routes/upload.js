3754d545efc80944041c7295b112b0e8393c53af
Ian Paul <ian.paul@students.olin.edu>
2015-12-19 22:25:27 -0500

    testing

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,5 @@
 exports.index = function(req, res) {
- res.render('hike', {title: 'Upload test'});
+ res.render('upload', {title: 'Upload test', content: 'Your mother never loved you'});
 };
 
 exports.add_upload = function(req, res) {
